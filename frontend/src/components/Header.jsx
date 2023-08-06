fc60c4a8450b40e62b63a7e76da3363e5ced4a00
OtterSloth <76695983+OtterSloth@users.noreply.github.com>
2023-08-06 18:48:44 -0400

    Remove unused files

--- frontend/src/components/Header.jsx
+++
@@ -1,4 +0,0 @@
-const Header = () => {
-  return <h1>Score Keeper</h1>
-}
-export default Header
--- frontend/src/pages/ListScores.jsx
+++
@@ -1,4 +0,0 @@
-const ListScores = () => {
-  return <h2>ListScores</h2>
-}
-export default ListScores
