c5362722127acb26d2ce46a1d5a2d716daf9a6b0
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2023-09-08 12:00:15 -0400

    Create constant values

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,33 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const express = require("express")
-const cors = require("cors")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
 
-const mongoose = require("mongoose")
+// Constants
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT;
 
 // Middleware
-app.use(cors())
-app.use(express.json())
-const logger = require("./middleware/logger")
-app.use(logger)
+app.use(cors());
+app.use(express.json());
+const logger = require("./middleware/logger");
+app.use(logger);
 
 // Routes
-const scoreRoutes = require("./routes/scores")
-const playerRoutes = require("./routes/players")
-const gameRoutes = require("./routes/games")
-app.use("/api/scores", scoreRoutes)
-app.use("/api/players", playerRoutes)
-app.use("/api/games", gameRoutes)
-
-const PORT = process.env.PORT
+const scoreRoutes = require("./routes/scores");
+const playerRoutes = require("./routes/players");
+const gameRoutes = require("./routes/games");
+app.use("/api/scores", scoreRoutes);
+app.use("/api/players", playerRoutes);
+app.use("/api/games", gameRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(MONGO_URI)
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`App listening on port ${PORT}...`)
-    })
+      console.log(`Server listening on port ${PORT}...`);
+    });
   })
-  .catch((err) => console.log(err.message))
+  .catch((err) => console.log(err.message));
